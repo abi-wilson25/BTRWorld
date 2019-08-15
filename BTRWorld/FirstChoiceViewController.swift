2622609168f72d9f10b02c0da0e99761a8eba2d6
Apple <apple@Apples-MacBook-Pro-2.local>
2019-08-15 09:02:05 -0700

    3

---
+++ BTRWorld/FirstChoiceViewController.swift
@@ -0,0 +1,45 @@
+//
+//  FirstChoiceViewController.swift
+//  BTRWorld
+//
+//  Created by Apple on 8/15/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class FirstChoiceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+ 
+
+    
+// firstChoiceTapped(_ sender: Any) {
+//    
+//        let alertController = UIAlertController(title: "Notice!", message: "These choices will affect your story and change the outcome. Be careful and choose wisely, because they choices cannot be changed afterwards", preferredStyle: UIAlertController.Style.alert)
+//            alertController.addAction(UIAlertAction(title: "I understand", style: UIAlertAction.Style.default, handler: nil))
+//            present(alertController, animated: true, completion: nil)}
+//    
+ 
+    
+    
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
---
+++ CrasshhViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CrasshhViewController.swift
+//  BTRWorld
+//
+//  Created by Apple on 8/15/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class CrasshhViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
