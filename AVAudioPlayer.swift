136466c4f7173f822031a97ea5576502e708e94d
Apple <apple@Apples-MacBook-Pro-2.local>
2019-08-15 13:46:17 -0700

    we tried

---
+++ AVAudioPlayer.swift
@@ -0,0 +1,38 @@
+//
+//  AVAudioPlayer.swift
+//  BTRWorld
+//
+//  Created by Apple on 8/15/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class AVAudioPlayer: NSObject {
+    
+
+            //  var audioPlayer = AVAudioPlayer()
+            var dog = true
+            let path = Bundle.main.path(forResource: "Boyfriend.mp3", ofType: "mp3")!
+            var one = 1
+            @IBAction func music(_ sender: Any) {
+                
+                if one == 1 {
+                    let audioPlayer = try! AVAudioPlayer()
+                    
+                }
+                    
+                else {
+                    print("error")
+                }
+                 func viewDidLoad() {
+                  viewDidLoad()
+                }
+                
+                
+            }
+        }
+
+        
+
+
---
+++ BTRWorld/music.swift
@@ -0,0 +1,37 @@
+//
+//  Music.swift
+//  BTRWorld
+//
+//  Created by Apple on 8/15/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+
+import UIKit
+import AVFoundation
+class music: UIViewController{
+    class AVAudioPlayer : NSObject {
+  //  var audioPlayer = AVAudioPlayer()
+        var dog = true
+        let path = Bundle.main.path(forResource: "Boyfriend.mp3", ofType: "mp3")!
+        var one = 1
+@IBAction func music(_ sender: Any) {
+    
+    if one == 1 {
+        let audioPlayer = try! AVAudioPlayer()
+    
+    }
+        
+    else {
+        print("error")
+    }
+    
+
+   
+    }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
